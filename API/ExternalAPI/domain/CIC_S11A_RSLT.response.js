c53f5874262efcf48c7dc7507d83b178e1a1f0f2
luannt2410 <luannt@infoplusvn.com>
2020-01-14 11:37:29 +0700

    external-default fr response s11a report

--- API/ExternalAPI/domain/CIC_S11A_RSLT.response.js
+++ API/ExternalAPI/domain/CIC_S11A_RSLT.response.js
@@ -31,24 +31,24 @@ module.exports = function CIC_S11A_RSLTResponse(cics11aRSLTRequest, response, ou
     } = outputCicrptMain;
 
     this.requestData = new cics11aRSLTReq(cics11aRSLTRequest);
-    this.responseTime = responseTime ? responseTime : "";
-    this.responseCode = responseCode ? responseCode : "";
-    this.responseMessage = responseMessage ? responseMessage : "";
-    this.scrapingStatusCode = R_ERRYN ? R_ERRYN : "";
-    this.cicReportRequestDate = S_DTIM ? S_DTIM : "";
-    this.cicReportResponseDate = R_DTIM ? R_DTIM : "";
-    this.cicReportInquiryUserId = S_REQ_STATUS ? S_REQ_STATUS : "";
-    this.cicInquiryFiName = INQ_OGZ_NM;
-    this.cicInquiryFiAddress = INQ_OGZ_ADDR;
-    this.cicUserName = INQ_USER_NM;
-    this.cicInquiryCode = INQ_CD;
-    this.cicReportInquiryDateTime = INQ_DTIM;
-    this.cicReportResultDateTime = RPT_SEND_DTIM;
-    this.name = PSN_NM;
-    this.cicId = CIC_ID;
-    this.address = PSN_ADDR;
-    this.nationalId = NATL_ID;
-    this.docIdEvidance = OTR_IDEN_EVD;
+    this.responseTime = responseTime ? responseTime : '';
+    this.responseCode = responseCode ? responseCode : '';
+    this.responseMessage = responseMessage ? responseMessage : '';
+    this.scrapingStatusCode = R_ERRYN ? R_ERRYN : '';
+    this.cicReportRequestDate = S_DTIM ? S_DTIM : '';
+    this.cicReportResponseDate = R_DTIM ? R_DTIM : '';
+    this.cicReportInquiryUserId = S_REQ_STATUS ? S_REQ_STATUS : '';
+    this.cicInquiryFiName = INQ_OGZ_NM ? INQ_OGZ_NM : '';
+    this.cicInquiryFiAddress = INQ_OGZ_ADDR ? INQ_OGZ_ADDR : '';
+    this.cicUserName = INQ_USER_NM ? INQ_USER_NM : '';
+    this.cicInquiryCode = INQ_CD ? INQ_CD : '';
+    this.cicReportInquiryDateTime = INQ_DTIM ? INQ_DTIM : '';
+    this.cicReportResultDateTime = RPT_SEND_DTIM ? RPT_SEND_DTIM : '';
+    this.name = PSN_NM ? PSN_NM : '';
+    this.cicId = CIC_ID ? CIC_ID : '';
+    this.address = PSN_ADDR ? PSN_ADDR : '';
+    this.nationalId = NATL_ID ? NATL_ID : '';
+    this.docIdEvidance = OTR_IDEN_EVD ? OTR_IDEN_EVD : '';
 
 };
 
