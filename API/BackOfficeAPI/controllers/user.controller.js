d662c00e2bad8a383cdfdd18d3848876dec0806d
dthoanginfoplus <hoangdt@infoplus.com>
2020-02-04 16:35:59 +0700

    get + add + update + resetPassword User

--- API/BackOfficeAPI/controllers/user.controller.js
+++ API/BackOfficeAPI/controllers/user.controller.js
@@ -9,10 +9,10 @@ const defaultPW = 'nice1234';
 
 
 exports.getUserInfo = async function (req, res) {
-    var INOUT_GB = req.query.userClass;
-    var USER_ID = req.query.userID;
-    var USER_NM = req.query.userName;
-    var CUST_CD = req.query.orgCode;
+    var userClass = req.query.userClass;
+    var userID = req.query.userID;
+    var userName = req.query.userName;
+    var orgCode = req.query.orgCode;
     var currentLocation = req.query.currentLocation;
     var limitRow = req.query.limitRow;
 
@@ -24,18 +24,18 @@ exports.getUserInfo = async function (req, res) {
     ADDR as ADDR,
     EMAIL as EMAIL,
     TEL_NO_MOBILE as TEL_NO_MOBILE,
-    to_char(to_date(SYS_DTIM, 'YYYY/MM/DD HH:MI:SS'),'mm/dd/yyyy hh:mi:ss') as SYS_DTIM,
+    to_char(to_date(SYS_DTIM, 'YYYY/MM/DD HH24:MI:SS'),'mm/dd/yyyy hh24:mi:ss') as SYS_DTIM,
     to_char(to_date(VALID_START_DT, 'yyyymmdd'),'mm/dd/yyyy') as VALID_START_DT,
     to_char(to_date(VALID_END_DT, 'yyyymmdd'),'mm/dd/yyyy') as VALID_END_DT,
     WORK_ID as WORK_ID `;
     var SQL_FROM = 'FROM TB_ITUSER ';
-    var SQL_WHERE_SEARCH = 'WHERE INOUT_GB LIKE :INOUT_GB ' +
-                                'OR USER_ID LIKE :USER_ID ' +
-                                'OR USER_NM LIKE :USER_NM ' +
-                                'OR CUST_CD LIKE :CUST_CD ';
+    var SQL_WHERE_SEARCH = 'WHERE INOUT_GB LIKE :userClass ' +
+                                'OR USER_ID LIKE :userID ' +
+                                'OR USER_NM LIKE :userName ' +
+                                'OR CUST_CD LIKE :orgCode ';
     var SQL_LIMIT = 'OFFSET :currentLocation ROWS FETCH NEXT :limitRow ROWS ONLY ';
 
-    if (_.isEmpty(INOUT_GB) && _.isEmpty(USER_ID) && _.isEmpty(USER_NM) && _.isEmpty(CUST_CD)) {
+    if (_.isEmpty(userClass) && _.isEmpty(userID) && _.isEmpty(userName) && _.isEmpty(orgCode)) {
         let sql = SQL_SELECT + SQL_FROM + SQL_LIMIT;
         let param = {
             currentLocation,
@@ -45,10 +45,10 @@ exports.getUserInfo = async function (req, res) {
     } else {
         let sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_LIMIT;
         let param = {
-            INOUT_GB,
-            USER_ID,
-            USER_NM,
-            CUST_CD,
+            userClass,
+            userID,
+            orgCode,
+            userName,
             currentLocation,
             limitRow
         };
@@ -58,38 +58,38 @@ exports.getUserInfo = async function (req, res) {
 
 exports.createUser = async function (req, res) {
     try {
-    var INOUT_GB = req.body.userClass;
-    var USER_NM = req.body.userName;
-    var CUST_CD = req.body.orgCode;
-    var VALID_START_DT = (_.isEmpty(req.body.validStartDT)) ? null: req.body.validStartDT.replace(/[^0-9 ]/g, "");
-    var VALID_END_DT = (_.isEmpty(req.body.validEndDT)) ? null: req.body.validEndDT.replace(/[^0-9 ]/g, "");
-    var TEL_NO_MOBILE = req.body.phone;
-    var ADDR = req.body.address;
-    var EMAIL = req.body.email;
-    var SYS_DTIM = req.body.finalOperaDate.replace(/[^0-9 ]/g, "");;
-    var WORK_ID = req.body.workID;
-    var USER_PW = bcrypt.hashSync(defaultPW, saltRounds);
+    var userClass = req.body.userClass;
+    var userName = req.body.userName;
+    var orgCode = req.body.orgCode;
+    var validStartDT = (_.isEmpty(req.body.validStartDT)) ? null: req.body.validStartDT.replace(/[^0-9 ]/g, "");
+    var validEndDT = (_.isEmpty(req.body.validEndDT)) ? null: req.body.validEndDT.replace(/[^0-9 ]/g, "");
+    var phone = req.body.phone;
+    var address = req.body.address;
+    var email = req.body.email;
+    var finalOperaDate = req.body.finalOperaDate.replace(/[^0-9 ]/g, "");;
+    var workID = req.body.workID;
+    var userPW = bcrypt.hashSync(defaultPW, saltRounds);
 
 
-    if(_.isEmpty(USER_NM) || _.isEmpty(CUST_CD) ||
-        _.isEmpty(SYS_DTIM) || _.isEmpty(INOUT_GB)) {
+    if(_.isEmpty(userName) || _.isEmpty(orgCode) ||
+        _.isEmpty(finalOperaDate) || _.isEmpty(userClass)) {
         res.status(500).send({message: 'User Classification, User Name, Final Operation Date, Organization Code require not blank'});
     } else {
             var param = {
-                USER_NM,
-                CUST_CD,
-                INOUT_GB,
-                USER_PW,
-                VALID_START_DT,
-                VALID_END_DT,
-                TEL_NO_MOBILE,
-                ADDR,
-                EMAIL,
-                SYS_DTIM,
-                WORK_ID
+                userName,
+                orgCode,
+                userClass,
+                userPW,
+                validStartDT,
+                validEndDT,
+                phone,
+                address,
+                email,
+                finalOperaDate,
+                workID
             };
             let SQL = 'INSERT INTO ADMIN.TB_ITUSER (USER_NM, CUST_CD, INOUT_GB, USER_PW, VALID_START_DT, VALID_END_DT, TEL_NO_MOBILE, ADDR, EMAIL, SYS_DTIM, WORK_ID) ' +
-                'VALUES(:USER_NM, :CUST_CD, :INOUT_GB, :USER_PW, :VALID_START_DT, :VALID_END_DT, :TEL_NO_MOBILE, :ADDR, :EMAIL, :SYS_DTIM, :WORK_ID)';
+                'VALUES(:userName, :orgCode, :userClass, :userPW, :validStartDT, :validEndDT, :phone, :address, :email, :finalOperaDate, :workID)';
 
             oracelService.queryOracel(res, SQL, param, optionAutoCommit);
         }
@@ -98,52 +98,52 @@ exports.createUser = async function (req, res) {
     }
 };
 exports.updateUser = async function (req, res) {
-    var INOUT_GB = req.body.userClass;
-    var USER_NM = req.body.userName;
-    var CUST_CD = req.body.orgCode;
-    var VALID_START_DT = (_.isEmpty(req.body.validStartDT)) ? null: req.body.validStartDT.replace(/[^0-9 ]/g, "");
-    var VALID_END_DT = (_.isEmpty(req.body.validEndDT)) ? null: req.body.validEndDT.replace(/[^0-9 ]/g, "");
-    var TEL_NO_MOBILE = req.body.phone;
-    var ADDR = req.body.address;
-    var EMAIL = req.body.email;
-    var USER_ID = req.body.userID;
+    var userClass = req.body.userClass;
+    var userName = req.body.userName;
+    var orgCode = req.body.orgCode;
+    var validStartDT = (_.isEmpty(req.body.validStartDT)) ? null: req.body.validStartDT.replace(/[^0-9 ]/g, "");
+    var validEndDT = (_.isEmpty(req.body.validEndDT)) ? null: req.body.validEndDT.replace(/[^0-9 ]/g, "");
+    var phone = req.body.phone;
+    var address = req.body.address;
+    var email = req.body.email;
+    var userID = req.body.userID;
 
-    if(_.isEmpty(USER_NM) || _.isEmpty(CUST_CD) ||
-         _.isEmpty(INOUT_GB)) {
+    if(_.isEmpty(userName) || _.isEmpty(orgCode) ||
+         _.isEmpty(userClass)) {
         res.status(500).send({message: 'User Classification, User Name, Organization Code require not blank'});
     } else {
         var param = {
-            USER_NM,
-            INOUT_GB,
-            CUST_CD,
-            VALID_START_DT,
-            VALID_END_DT,
-            TEL_NO_MOBILE,
-            ADDR,
-            EMAIL,
-            USER_ID,
+            userName,
+            userClass,
+            orgCode,
+            validStartDT,
+            validEndDT,
+            phone,
+            address,
+            email,
+            userID,
         };
 
         let SQL = 'UPDATE ADMIN.TB_ITUSER SET' +
-            ' USER_NM=:USER_NM, CUST_CD=:CUST_CD, INOUT_GB=:INOUT_GB, VALID_START_DT=:VALID_START_DT, VALID_END_DT=:VALID_END_DT, TEL_NO_MOBILE=:TEL_NO_MOBILE, ADDR=:ADDR, EMAIL=:EMAIL' +
-            ' WHERE USER_ID=:USER_ID ';
+            ' USER_NM=:userName, CUST_CD=:orgCode, INOUT_GB=:userClass, VALID_START_DT=:validStartDT, VALID_END_DT=:validEndDT, TEL_NO_MOBILE=:phone, ADDR=:address, EMAIL=:email' +
+            ' WHERE USER_ID=:userID ';
         oracelService.queryOracel(res, SQL, param, optionAutoCommit);
     }
 };
 
 exports.resetPassword = async function (req, res) {
-    var USER_ID = req.body.userID;
-    var USER_PW = bcrypt.hashSync(defaultPW, saltRounds);
+    var userID = req.body.userID;
+    var userPW = bcrypt.hashSync(defaultPW, saltRounds);
 
-    if(_.isEmpty(USER_ID)) {
+    if(_.isEmpty(userID)) {
         res.status(500).send({message: 'User ID provide blank!'})
     } else {
         var param = {
-            USER_ID,
-            USER_PW
+            userID,
+            userPW
         };
 
-        let SQL = 'UPDATE ADMIN.TB_ITUSER SET USER_PW=:USER_PW WHERE USER_ID=:USER_ID ';
+        let SQL = 'UPDATE ADMIN.TB_ITUSER SET USER_PW=:userPW WHERE USER_ID=:userID ';
         oracelService.queryOracel(res, SQL, param, optionAutoCommit);
     }
 };
