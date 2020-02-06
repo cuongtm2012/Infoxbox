3edf242bada9b2c3423c63e5602770887104142e
Tuan Anh <anhnt@infoplusvn.com>
2020-02-06 11:10:40 +0700

    06022020 remove getdata header in response

--- API/ExternalAPI/domain/CIC_MACR_RQST.response.js
+++ API/ExternalAPI/domain/CIC_MACR_RQST.response.js
@@ -1,5 +1,3 @@
-// var BaseResponse = require('./Base.response');
-var cicMacrRQSTReq = require('./CIC_MACR_RQST.request');
 
 module.exports = function CIC_MACR_RQSTResponse(cicMacrRQSTRequest, response) {
 
@@ -10,7 +8,35 @@ module.exports = function CIC_MACR_RQSTResponse(cicMacrRQSTRequest, response) {
         responseCode
     } = response;
 
-    this.requestData = new cicMacrRQSTReq(cicMacrRQSTRequest);
+    const { 
+        fiSessionKey,
+        fiCode,
+        taskCode,
+        name,
+        mobilePhoneNumber,
+        taxCode,
+        natId,
+        oldNatId,
+        passportNumber,
+        cicId,
+        inquiryDate,
+        infoProvConcent
+    } = cicMacrRQSTRequest;
+
+    this.fiSessionKey = fiSessionKey ? fiSessionKey : "";
+    this.fiCode = fiCode;
+    this.taskCode = taskCode;
+    this.name = name;
+    this.mobilePhoneNumber = mobilePhoneNumber;
+    this.taxCode = taxCode ? taxCode : "";
+    this.natId = natId ? natId : "";
+    this.oldNatId = oldNatId ? oldNatId : "";
+    this.passportNumber = passportNumber ? passportNumber : "";
+    this.cicId = cicId ? cicId : "";
+    this.inquiryDate = inquiryDate ? inquiryDate : "" ;
+    this.infoProvConcent = infoProvConcent;
+
+    this.niceSessionKey = niceSessionKey;
     this.niceSessionKey = niceSessionKey ? niceSessionKey : "";
     this.responseTime = responseTime ? responseTime : "";
     this.responseCode = responseCode ? responseCode : "";
