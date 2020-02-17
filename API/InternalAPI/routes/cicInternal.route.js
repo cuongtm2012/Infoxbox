e921b18af2287e2bc579414a7aec3367fb33c6c8
luannt2410 <luannt@infoplusvn.com>
2020-02-17 14:36:23 +0700

    internal delete file don't use

--- API/InternalAPI/routes/cicInternal.route.js
+++ API/InternalAPI/routes/cicInternal.route.js
@@ -2,12 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var internalCIC_controller = require('../controllers/internalCIC.controller');
-// var manualCaptcha_controller = require('../controllers/manualCaptcha.controller');
 
 // request to cic server
 router.post('/cic', internalCIC_controller.internalCIC);
 router.post('/cicB0003', internalCIC_controller.internalCICB0003);
 
-// router.post('/manualCaptcha', manualCaptcha_controller.internalCICManualCaptcha);
 
 module.exports = router;
