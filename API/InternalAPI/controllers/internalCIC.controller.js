b9980eb4742244bdb8f712fc72138c4560329be3
luannt2410 <luannt@infoplusvn.com>
2020-02-17 16:36:43 +0700

    update condition B0001

--- API/InternalAPI/controllers/internalCIC.controller.js
+++ API/InternalAPI/controllers/internalCIC.controller.js
@@ -31,7 +31,7 @@ exports.internalCIC = function (req, res, next) {
                 }
 
                 // update process status = 04 update process completed
-                else if (!_.isEmpty(body.data.outJson.outB0001) && body.data.outJson.outB0001.errYn == "N" && !_.isEmpty(body.data.outJson.outB0002.cicNo)) {
+                else if (!_.isEmpty(body.data.outJson.outB0001) && (body.data.outJson.outB0001.errYn == "N" || _.isEmpty(body.data.outJson.outB0001.errYn)) && !_.isEmpty(body.data.outJson.outB0002.cicNo)) {
                     //update process status = 04, sucecssful recieve response from scraping service
                     cicService.updateCICReportInquirySuccessful(req.body, res).then(resultUpdated => {
                         console.log("CIC report inquiry successful!");
