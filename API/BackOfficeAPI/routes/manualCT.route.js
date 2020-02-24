c938030c1933f091da6c21c6f38e918a04db86e3
dthoanginfoplus <hoangdt@infoplus.com>
2020-02-24 18:05:58 +0700

    Search Data for B1003 manual captcha

--- API/BackOfficeAPI/routes/manualCT.route.js
+++ API/BackOfficeAPI/routes/manualCT.route.js
@@ -6,5 +6,5 @@ var verifyToken = require('../util/verifyToken');
 var manualCaptcha = require('../controllers/manualCaptcha.conroller');
 
 router.get('/getListB003', verifyToken ,  manualCaptcha.getListB003);
-router.get('/getListB003',   manualCaptcha.getListB003);
+router.get('/getListB001',   manualCaptcha.getListForB001);
 module.exports = router;
