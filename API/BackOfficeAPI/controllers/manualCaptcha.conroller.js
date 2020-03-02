d415b30a16cf15ed296769439bdee1c1fb23aca5
dthoanginfoplus <hoangdt@infoplus.com>
2020-03-02 15:18:32 +0700

    search like%% manual captcha

--- API/BackOfficeAPI/controllers/manualCaptcha.conroller.js
+++ API/BackOfficeAPI/controllers/manualCaptcha.conroller.js
@@ -26,7 +26,7 @@ exports.getListB003 = async function (req, res) {
 };
 
 exports.getListForB001 = async function (req, res) {
-    var niceSsID = req.query.niceSsID;
+    var niceSsID = req.query.niceSsID ? '%' + req.query.niceSsID + '%' : '';
     var currentLocation = req.query.currentLocation;
     var limitRow = req.query.limitRow;
     var SQL_SELECT = `SELECT
@@ -91,5 +91,273 @@ exports.getListForB001 = async function (req, res) {
 };
 
 exports.getListB002 = async function (req, res) {
-    var niceSsID = '%' + req.query.niceSsID + '%';
+    var niceSsID = req.query.niceSsID ? '%' + req.query.niceSsID + '%' : '';
+    var orgCode = req.query.orgCode ? '%' + req.query.orgCode + '%' : '';
+    var productCode = req.query.productCode ? '%' + req.query.productCode + '%' : '';
+    var CICNumber = req.query.CICNumber ? '%' + req.query.CICNumber + '%' : '';
+    var inqDateFrom = (_.isEmpty(req.query.inqDateFrom)) ? '': req.query.inqDateFrom.replace(/[^0-9 ]/g, "");
+    var inqDateTo = (_.isEmpty(req.query.inqDateTo)) ? '': req.query.inqDateTo.replace(/[^0-9 ]/g, "");
+    var currentLocation = req.query.currentLocation;
+    var limitRow = req.query.limitRow;
+
+    var SQL_SELECT = `SELECT
+    TB_ITCUST.CUST_GB as CUST_GB, 
+    TB_ITCUST.CUST_CD as CUST_CD, 
+    TB_ITCUST.CUST_NM as CUST_NM, 
+    TB_ITCODE.CODE_NM as CODE_NM,
+    TB_SCRPLOG.NICE_SSIN_ID as NICE_SSIN_ID,
+    TB_SCRPLOG.CUST_SSID_ID as CUST_SSID_ID,
+    TB_SCRPLOG.LOGIN_ID as LOGIN_ID,
+    TB_SCRPLOG.LOGIN_PW as LOGIN_PW,
+    TB_SCRPLOG.GDS_CD as GDS_CD,
+    TB_SCRPLOG.LOGIN_ID as LOGIN_ID,
+    TB_SCRPLOG.LOGIN_PW as LOGIN_PW,
+    TB_SCRPLOG.NATL_ID as NATL_ID,
+    TB_SCRPLOG.OLD_NATL_ID as OLD_NATL_ID,
+    TB_SCRPLOG.CUST_ID as CUST_ID,
+    TB_SCRPLOG.TAX_ID as TAX_ID,
+    TB_SCRPLOG.PSPT_NO as PSPT_NO,
+    TB_SCRPLOG.OTR_ID as OTR_ID,
+    TB_SCRPLOG.CIC_ID as CIC_ID,
+    TB_SCRPLOG.CIC_USED_ID as CIC_USED_ID,
+    TB_SCRPLOG.CIC_GDS_CD as CIC_GDS_CD,
+    TB_SCRPLOG.PSN_NM as PSN_NM,
+    TB_SCRPLOG.TEL_NO_MOBILE as TEL_NO_MOBILE,
+    TB_SCRPLOG.INQ_DTIM as INQ_DTIM,
+    TB_SCRPLOG.AGR_FG as AGR_FG,
+    TB_SCRPLOG.SCRP_STAT_CD as SCRP_STAT_CD,
+    TB_SCRPLOG.RSP_CD as RSP_CD,
+    TB_SCRPLOG.RSP_CD as RSP_CD,
+    to_char(to_date(TB_SCRPLOG.SYS_DTIM, 'YYYY/MM/DD HH24:MI:SS'),'mm/dd/yyyy hh24:mi:ss') as SYS_DTIM,
+    to_char(to_date(TB_SCRPLOG.INQ_DTIM, 'YYYY/MM/DD'),'mm/dd/yyyy') as INQ_DTIM,
+    TB_SCRPLOG.SCRP_MOD_CD as SCRP_MOD_CD,
+    TB_SCRPLOG.SCRP_REQ_DTIM as SCRP_REQ_DTIM,
+    TB_SCRPLOG.WORK_ID as WORK_ID  `;
+    var SQL_FROM = 'FROM TB_SCRPLOG ';
+    var SQL_JOIN = 'LEFT JOIN TB_ITCUST ON TB_SCRPLOG.CUST_CD = TB_ITCUST.CUST_CD ' +
+        'LEFT JOIN TB_ITCODE ON TB_SCRPLOG.GDS_CD = TB_ITCODE.CODE ';
+    var SQL_ORDER_BY = `ORDER BY CASE WHEN TB_SCRPLOG.INQ_DTIM IS NOT NULL THEN 1 ELSE 0 END DESC, TB_SCRPLOG.INQ_DTIM DESC `;
+    var SQL_LIMIT = 'OFFSET :currentLocation ROWS FETCH NEXT :limitRow ROWS ONLY ';
+
+    if (_.isEmpty(niceSsID) &&_.isEmpty(orgCode) && _.isEmpty(productCode) && _.isEmpty(CICNumber) && _.isEmpty(inqDateFrom) && _.isEmpty(inqDateTo)) {
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_ORDER_BY  + SQL_LIMIT;
+        let param = {
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+    if(orgCode && _.isEmpty(productCode) && _.isEmpty(CICNumber) && _.isEmpty(inqDateFrom) && _.isEmpty(inqDateTo)) {
+        let SQL_WHERE = 'WHERE TB_SCRPLOG.CUST_CD LIKE :orgCode ';
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            orgCode,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if( niceSsID &&_.isEmpty(orgCode) && _.isEmpty(productCode) && _.isEmpty(CICNumber) && _.isEmpty(inqDateFrom) && _.isEmpty(inqDateTo)) {
+        let SQL_WHERE = 'WHERE TB_SCRPLOG.NICE_SSIN_ID LIKE :niceSsID ';
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            niceSsID,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if(_.isEmpty(orgCode) && (productCode) && _.isEmpty(CICNumber) && _.isEmpty(inqDateFrom) && _.isEmpty(inqDateTo)) {
+        let SQL_WHERE = 'WHERE TB_SCRPLOG.GDS_CD LIKE :productCode ';
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            productCode,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if(_.isEmpty(orgCode) && _.isEmpty(productCode) && (CICNumber) && _.isEmpty(inqDateFrom) && _.isEmpty(inqDateTo)) {
+        let SQL_WHERE = 'WHERE TB_SCRPLOG.CIC_ID LIKE :CICNumber ';
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            CICNumber,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if(_.isEmpty(orgCode) && _.isEmpty(productCode) && _.isEmpty(CICNumber) && (inqDateFrom) && (inqDateTo)) {
+        let SQL_WHERE = `WHERE to_date(TB_SCRPLOG.INQ_DTIM, 'YYYY/MM/DD') BETWEEN to_date(:inqDateFrom, 'YYYY/MM/DD') AND to_date(:inqDateTo, 'YYYY/MM/DD') `;
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            inqDateFrom,
+            inqDateTo,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if((orgCode) && (productCode) && _.isEmpty(CICNumber) && _.isEmpty(inqDateFrom) && _.isEmpty(inqDateTo)) {
+        let SQL_WHERE = 'WHERE TB_SCRPLOG.CUST_CD LIKE :orgCode AND TB_SCRPLOG.GDS_CD LIKE :productCode ';
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            orgCode,
+            productCode,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if((orgCode) && _.isEmpty(productCode) && (CICNumber) && _.isEmpty(inqDateFrom) && _.isEmpty(inqDateTo)) {
+        let SQL_WHERE = 'WHERE TB_SCRPLOG.CUST_CD LIKE :orgCode AND TB_SCRPLOG.CIC_ID LIKE :CICNumber ';
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            orgCode,
+            CICNumber,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if((orgCode) && _.isEmpty(productCode) && _.isEmpty(CICNumber) && (inqDateFrom) && (inqDateTo)) {
+        let SQL_WHERE = `WHERE TB_SCRPLOG.CUST_CD LIKE :orgCode AND to_date(TB_SCRPLOG.INQ_DTIM, 'YYYY/MM/DD') BETWEEN to_date(:inqDateFrom, 'YYYY/MM/DD') AND to_date(:inqDateTo, 'YYYY/MM/DD') `;
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            orgCode,
+            inqDateFrom,
+            inqDateTo,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if(_.isEmpty(orgCode) && (productCode) && (CICNumber) && _.isEmpty(inqDateFrom) && _.isEmpty(inqDateTo)) {
+        let SQL_WHERE = 'WHERE TB_SCRPLOG.GDS_CD LIKE :productCode AND TB_SCRPLOG.CIC_ID LIKE :CICNumber ';
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            productCode,
+            CICNumber,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if(_.isEmpty(orgCode) && (productCode) && _.isEmpty(CICNumber) && (inqDateFrom) && (inqDateTo)) {
+        let SQL_WHERE = `WHERE TB_SCRPLOG.GDS_CD LIKE :productCode AND 
+         to_date(TB_SCRPLOG.INQ_DTIM, 'YYYY/MM/DD') BETWEEN to_date(:inqDateFrom, 'YYYY/MM/DD') AND to_date(:inqDateTo, 'YYYY/MM/DD') `;
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            productCode,
+            inqDateFrom,
+            inqDateTo,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if(_.isEmpty(orgCode) && _.isEmpty(productCode) && (CICNumber) && (inqDateFrom) && (inqDateTo)) {
+        let SQL_WHERE = `WHERE TB_SCRPLOG.CIC_ID LIKE :CICNumber :orgCode AND 
+         to_date(TB_SCRPLOG.INQ_DTIM, 'YYYY/MM/DD') BETWEEN to_date(:inqDateFrom, 'YYYY/MM/DD') AND to_date(:inqDateTo, 'YYYY/MM/DD') `;
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            CICNumber,
+            inqDateFrom,
+            inqDateTo,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if((orgCode) && (productCode) && (CICNumber) && _.isEmpty(inqDateFrom) && _.isEmpty(inqDateTo)) {
+        let SQL_WHERE = `WHERE TB_SCRPLOG.CUST_CD LIKE :orgCode AND 
+                               TB_SCRPLOG.GDS_CD LIKE :productCode AND 
+                               TB_SCRPLOG.CIC_ID LIKE :CICNumber  `;
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            orgCode,
+            productCode,
+            CICNumber,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if((orgCode) && (productCode) && _.isEmpty(CICNumber) && (inqDateFrom) && (inqDateTo)) {
+        let SQL_WHERE = `WHERE TB_SCRPLOG.CUST_CD LIKE :orgCode AND 
+                               TB_SCRPLOG.GDS_CD LIKE :productCode AND 
+                               to_date(TB_SCRPLOG.INQ_DTIM, 'YYYY/MM/DD') BETWEEN to_date(:inqDateFrom, 'YYYY/MM/DD') AND to_date(:inqDateTo, 'YYYY/MM/DD')  `;
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            orgCode,
+            productCode,
+            inqDateFrom,
+            inqDateTo,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if((orgCode) && _.isEmpty(productCode) && (CICNumber) && (inqDateFrom) && (inqDateTo)) {
+        let SQL_WHERE = `WHERE TB_SCRPLOG.CUST_CD LIKE :orgCode AND 
+                               TB_SCRPLOG.CIC_ID LIKE :CICNumber AND 
+                               to_date(TB_SCRPLOG.INQ_DTIM, 'YYYY/MM/DD') BETWEEN to_date(:inqDateFrom, 'YYYY/MM/DD') AND to_date(:inqDateTo, 'YYYY/MM/DD')  `;
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            orgCode,
+            CICNumber,
+            inqDateFrom,
+            inqDateTo,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if(_.isEmpty(orgCode) && (productCode) && (CICNumber) && (inqDateFrom) && (inqDateTo)) {
+        let SQL_WHERE = `WHERE TB_SCRPLOG.GDS_CD LIKE :productCode AND 
+                               TB_SCRPLOG.CIC_ID LIKE :CICNumber AND 
+                               to_date(TB_SCRPLOG.INQ_DTIM, 'YYYY/MM/DD') BETWEEN to_date(:inqDateFrom, 'YYYY/MM/DD') AND to_date(:inqDateTo, 'YYYY/MM/DD')  `;
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            productCode,
+            CICNumber,
+            inqDateFrom,
+            inqDateTo,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
+
+    if(_(orgCode) && (productCode) && (CICNumber) && (inqDateFrom) && (inqDateTo)) {
+        let SQL_WHERE = `WHERE TB_SCRPLOG.CUST_CD LIKE :orgCode AND
+                               TB_SCRPLOG.GDS_CD LIKE :productCode AND 
+                               TB_SCRPLOG.CIC_ID LIKE :CICNumber AND 
+                               to_date(TB_SCRPLOG.INQ_DTIM, 'YYYY/MM/DD') BETWEEN to_date(:inqDateFrom, 'YYYY/MM/DD') AND to_date(:inqDateTo, 'YYYY/MM/DD')  `;
+        let sql = SQL_SELECT + SQL_FROM + SQL_JOIN + SQL_WHERE + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            orgCode,
+            productCode,
+            CICNumber,
+            inqDateFrom,
+            inqDateTo,
+            currentLocation,
+            limitRow
+        };
+        return oracelService.queryOracel(res, sql, param, optionFormatObj);
+    }
 };
--- API/BackOfficeAPI/routes/manualCT.route.js
+++ API/BackOfficeAPI/routes/manualCT.route.js
@@ -7,4 +7,5 @@ var manualCaptcha = require('../controllers/manualCaptcha.conroller');
 
 router.get('/getListB003', verifyToken ,  manualCaptcha.getListB003);
 router.get('/getListB001',   manualCaptcha.getListForB001);
+router.get('/getListB002',   manualCaptcha.getListB002);
 module.exports = router;
