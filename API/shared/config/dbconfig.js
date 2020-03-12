9a1b8c5c6648890e6f4b01896b142841d25789e9
luannt@infoplusvn.com <luannt@infoplusvn.com>
2020-03-12 18:25:24 +0700

    config

--- API/shared/config/dbconfig.js
+++ API/shared/config/dbconfig.js
@@ -6,12 +6,12 @@ module.exports = {
     // string (not recommended), or it could be prompted for.
     // Alternatively use External Authentication so that no password is
     // needed.
-    password      : "admin200%scrp",
+    password      : "admin100%scrp",
   
     // For information on connection strings see:
     // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings
     // connectString : "localhost/infobox",
-    connectString : "10.84.243.126:1522/VNDEV",
+   connectString : "10.84.23.122:1577/VNDPG",
   
     // Setting externalAuth is optional.  It defaults to false.  See:
     // https://oracle.github.io/node-oracledb/doc/api.html#extauth
