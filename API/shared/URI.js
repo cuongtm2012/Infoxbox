b79e6b66675c432c2275dd807524e43bd81e9cfb
luannt2410 <luannt@infoplusvn.com>
2020-03-25 14:56:34 +0700

    change port

--- API/shared/URI.js
+++ API/shared/URI.js
@@ -3,9 +3,9 @@ var URI = {
 
     cicInternalJson: 'https://1.234.53.23:9401/rest/extJson',
 
-    internal_cic: 'https://localhost:3000/internal/cic',
-    internal_cicB0003: 'https://localhost:3000/internal/cicB0003',
-    internal_cicMobile: 'https://localhost:3000/internal/mobile'
+    internal_cic: 'https://localhost:3301/internal/cic',
+    internal_cicB0003: 'https://localhost:3301/internal/cicB0003',
+    internal_cicMobile: 'https://localhost:3301/internal/mobile'
 };
 
 module.exports = URI;
