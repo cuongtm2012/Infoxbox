fb5c21b80675fc3b6a1bb16d3a665937538f1b81
luannt2410 <luannt@infoplusvn.com>
2020-03-31 13:19:07 +0700

    back office remove redis

--- API/BackOfficeAPI/routes/auth.route.js
+++ API/BackOfficeAPI/routes/auth.route.js
@@ -7,7 +7,6 @@ var auth_controller = require('../controllers/auth.controller');
 
 router.post('/login',  auth_controller.login);
 router.post('/sendEmail',  auth_controller.sendEmail);
-router.post('/getCodeEmail',  auth_controller.getCodeEmail);
 router.post('/register',  auth_controller.register);
 module.exports = router;
 
