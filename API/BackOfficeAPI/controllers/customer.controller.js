611c56c7e279493d2d088aac49fb5630517c10bd
dthoanginfoplus <hoangdt@infoplus.com>
2020-04-08 11:35:56 +0700

    add status for TB_ITCUST + search

--- API/BackOfficeAPI/controllers/customer.controller.js
+++ API/BackOfficeAPI/controllers/customer.controller.js
@@ -8,12 +8,14 @@ exports.getCustInfo = async function (req, res) {
     var custClassicfication = req.query.custClassicfication ? '%' + req.query.custClassicfication + '%' : '';
     var cusCd = req.query.cusCd ? '%' + req.query.cusCd + '%' : '';
     var custNm = req.query.custNm ? '%' + req.query.custNm + '%' : '';
+    var status = req.query.status ? req.query.status : '';
     var currentLocation = req.query.currentLocation;
     var limitRow = req.query.limitRow;
     var SQL_SELECT = `SELECT 
     CUST_GB as CUST_GB, 
     CUST_CD as CUST_CD, 
     CUST_NM as CUST_NM, 
+    STATUS as STATUS, 
     CUST_NM_ENG as CUST_NM_ENG, 
     BRANCH_NM as BRANCH_NM, 
     BRANCH_NM_ENG as BRANCH_NM_ENG, 
@@ -26,7 +28,7 @@ exports.getCustInfo = async function (req, res) {
     var SQL_ORDER_BY = 'ORDER BY CUST_NM_ENG ';
     var SQL_LIMIT = 'OFFSET :currentLocation ROWS FETCH NEXT :limitRow ROWS ONLY ';
 
-    if (_.isEmpty(custClassicfication) && _.isEmpty(cusCd) && _.isEmpty(custNm)) {
+    if (_.isEmpty(custClassicfication) && _.isEmpty(cusCd) && _.isEmpty(custNm) && _.isEmpty(status)) {
         let sql = SQL_SELECT + SQL_FROM + SQL_ORDER_BY + SQL_LIMIT;
         let param = {
             currentLocation,
@@ -43,15 +45,16 @@ exports.getCustInfo = async function (req, res) {
 
     }
 
-    if ((custClassicfication) && (cusCd) && (custNm)) {
+    if ((custClassicfication) && (cusCd) && (custNm) && (status)) {
         let SQL_WHERE_SEARCH = 'WHERE CUST_GB LIKE :custClassicfication ' +
                                 'AND CUST_CD LIKE :cusCd ' +
-                                'AND CUST_NM_ENG LIKE :custNm ';
+                                'AND CUST_NM_ENG LIKE :custNm AND STATUS = :status ';
         sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
         let param = {
             custClassicfication,
             cusCd,
             custNm,
+            status,
             currentLocation,
             limitRow
         };
@@ -60,6 +63,7 @@ exports.getCustInfo = async function (req, res) {
             custClassicfication,
             cusCd,
             custNm,
+            status
         };
         let totalRow;
         let rowRs;
@@ -68,7 +72,7 @@ exports.getCustInfo = async function (req, res) {
         return res.status(200).send({count: totalRow, rowRs: rowRs});
     }
 
-    if ((custClassicfication) && _.isEmpty(cusCd) && _.isEmpty(custNm)) {
+    if ((custClassicfication) && _.isEmpty(cusCd) && _.isEmpty(custNm) && _.isEmpty(status)) {
         let SQL_WHERE_SEARCH = 'WHERE CUST_GB LIKE :custClassicfication ';
         sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
         let param = {
@@ -87,7 +91,7 @@ exports.getCustInfo = async function (req, res) {
         return res.status(200).send({count: totalRow, rowRs: rowRs});
     }
 
-    if (_.isEmpty(custClassicfication) && (cusCd) && _.isEmpty(custNm)) {
+    if (_.isEmpty(custClassicfication) && (cusCd) && _.isEmpty(custNm) && _.isEmpty(status)) {
         let SQL_WHERE_SEARCH = 'WHERE CUST_CD LIKE :cusCd ';
         sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
         let param = {
@@ -106,7 +110,7 @@ exports.getCustInfo = async function (req, res) {
         return res.status(200).send({count: totalRow, rowRs: rowRs});
     }
 
-    if (_.isEmpty(custClassicfication) && _.isEmpty(cusCd) && (custNm)) {
+    if (_.isEmpty(custClassicfication) && _.isEmpty(cusCd) && (custNm) && _.isEmpty(status)) {
         let SQL_WHERE_SEARCH = 'WHERE CUST_NM_ENG LIKE :custNm ';
         sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
         let param = {
@@ -125,7 +129,28 @@ exports.getCustInfo = async function (req, res) {
         return res.status(200).send({count: totalRow, rowRs: rowRs});
     }
 
-    if ((custClassicfication) && (cusCd) && _.isEmpty(custNm)) {
+
+
+    if (_.isEmpty(custClassicfication) && _.isEmpty(cusCd) && _.isEmpty(custNm) && (status)) {
+        let SQL_WHERE_SEARCH = 'WHERE STATUS LIKE :status ';
+        sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            status,
+            currentLocation,
+            limitRow
+        };
+        let sqlSearch = SQL_SELECT_COUNT + SQL_FROM + SQL_WHERE_SEARCH;
+        let paramSearch = {
+            status,
+        };
+        let totalRow;
+        let rowRs;
+        totalRow = await oracelService.queryGetTotalRow(res, sqlSearch, paramSearch, optionFormatObj);
+        rowRs = await oracelService.queryGetTotalRow(res, sql, param, optionFormatObj);
+        return res.status(200).send({count: totalRow, rowRs: rowRs});
+    }
+
+    if ((custClassicfication) && (cusCd) && _.isEmpty(custNm) && _.isEmpty(status)) {
         let SQL_WHERE_SEARCH = 'WHERE CUST_GB LIKE :custClassicfication AND CUST_CD LIKE :cusCd ';
         sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
         let param = {
@@ -146,7 +171,7 @@ exports.getCustInfo = async function (req, res) {
         return res.status(200).send({count: totalRow, rowRs: rowRs});
     }
 
-    if ((custClassicfication) && _.isEmpty(cusCd) && (custNm)) {
+    if ((custClassicfication) && _.isEmpty(cusCd) && (custNm)  && _.isEmpty(status)) {
         let SQL_WHERE_SEARCH = 'WHERE CUST_GB LIKE :custClassicfication AND CUST_NM_ENG LIKE :custNm ';
         sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
         let param = {
@@ -167,7 +192,28 @@ exports.getCustInfo = async function (req, res) {
         return res.status(200).send({count: totalRow, rowRs: rowRs});
     }
 
-    if (_.isEmpty(custClassicfication) && (cusCd) && (custNm)) {
+    if ((custClassicfication) && _.isEmpty(cusCd) && _.isEmpty(custNm)  && (status)) {
+        let SQL_WHERE_SEARCH = 'WHERE CUST_GB LIKE :custClassicfication AND STATUS LIKE :status ';
+        sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            custClassicfication,
+            status,
+            currentLocation,
+            limitRow
+        };
+        let sqlSearch = SQL_SELECT_COUNT + SQL_FROM + SQL_WHERE_SEARCH;
+        let paramSearch = {
+            custClassicfication,
+            status,
+        };
+        let totalRow;
+        let rowRs;
+        totalRow = await oracelService.queryGetTotalRow(res, sqlSearch, paramSearch, optionFormatObj);
+        rowRs = await oracelService.queryGetTotalRow(res, sql, param, optionFormatObj);
+        return res.status(200).send({count: totalRow, rowRs: rowRs});
+    }
+
+    if (_.isEmpty(custClassicfication) && (cusCd) && (custNm) && _.isEmpty(status)) {
         let SQL_WHERE_SEARCH = 'WHERE CUST_CD LIKE :cusCd AND CUST_NM_ENG LIKE :custNm ';
         sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
         let param = {
@@ -180,6 +226,144 @@ exports.getCustInfo = async function (req, res) {
         let paramSearch = {
             cusCd,
             custNm,
+        };
+        let totalRow;
+        let rowRs;
+        totalRow = await oracelService.queryGetTotalRow(res, sqlSearch, paramSearch, optionFormatObj);
+        rowRs = await oracelService.queryGetTotalRow(res, sql, param, optionFormatObj);
+        return res.status(200).send({count: totalRow, rowRs: rowRs});
+    }
+
+    if (_.isEmpty(custClassicfication) && (cusCd) && _.isEmpty(custNm) && (status)) {
+        let SQL_WHERE_SEARCH = 'WHERE CUST_CD LIKE :cusCd AND STATUS LIKE :status ';
+        sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            cusCd,
+            status,
+            currentLocation,
+            limitRow
+        };
+        let sqlSearch = SQL_SELECT_COUNT + SQL_FROM + SQL_WHERE_SEARCH;
+        let paramSearch = {
+            cusCd,
+            status,
+        };
+        let totalRow;
+        let rowRs;
+        totalRow = await oracelService.queryGetTotalRow(res, sqlSearch, paramSearch, optionFormatObj);
+        rowRs = await oracelService.queryGetTotalRow(res, sql, param, optionFormatObj);
+        return res.status(200).send({count: totalRow, rowRs: rowRs});
+    }
+
+    if (_.isEmpty(custClassicfication) && _.isEmpty(cusCd) && (custNm) && (status)) {
+        let SQL_WHERE_SEARCH = 'WHERE CUST_NM_ENG LIKE :custNm AND STATUS LIKE :status ';
+        sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            custNm,
+            status,
+            currentLocation,
+            limitRow
+        };
+        let sqlSearch = SQL_SELECT_COUNT + SQL_FROM + SQL_WHERE_SEARCH;
+        let paramSearch = {
+            custNm,
+            status,
+        };
+        let totalRow;
+        let rowRs;
+        totalRow = await oracelService.queryGetTotalRow(res, sqlSearch, paramSearch, optionFormatObj);
+        rowRs = await oracelService.queryGetTotalRow(res, sql, param, optionFormatObj);
+        return res.status(200).send({count: totalRow, rowRs: rowRs});
+    }
+
+
+    if ((custClassicfication) && (cusCd) && (custNm) && _.isEmpty(status)) {
+        let SQL_WHERE_SEARCH = 'WHERE CUST_NM_ENG LIKE :custNm AND CUST_GB LIKE :custClassicfication AND CUST_CD LIKE :cusCd ';
+        sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            custNm,
+            cusCd,
+            custClassicfication,
+            currentLocation,
+            limitRow
+        };
+        let sqlSearch = SQL_SELECT_COUNT + SQL_FROM + SQL_WHERE_SEARCH;
+        let paramSearch = {
+            custNm,
+            cusCd,
+            custClassicfication,
+        };
+        let totalRow;
+        let rowRs;
+        totalRow = await oracelService.queryGetTotalRow(res, sqlSearch, paramSearch, optionFormatObj);
+        rowRs = await oracelService.queryGetTotalRow(res, sql, param, optionFormatObj);
+        return res.status(200).send({count: totalRow, rowRs: rowRs});
+    }
+
+
+    if (_.isEmpty(custClassicfication) && (cusCd) && (custNm) && (status)) {
+        let SQL_WHERE_SEARCH = 'WHERE CUST_NM_ENG LIKE :custNm AND STATUS LIKE :status  AND CUST_CD LIKE :cusCd ';
+        sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            custNm,
+            cusCd,
+            status,
+            currentLocation,
+            limitRow
+        };
+        let sqlSearch = SQL_SELECT_COUNT + SQL_FROM + SQL_WHERE_SEARCH;
+        let paramSearch = {
+            custNm,
+            cusCd,
+            status,
+        };
+        let totalRow;
+        let rowRs;
+        totalRow = await oracelService.queryGetTotalRow(res, sqlSearch, paramSearch, optionFormatObj);
+        rowRs = await oracelService.queryGetTotalRow(res, sql, param, optionFormatObj);
+        return res.status(200).send({count: totalRow, rowRs: rowRs});
+    }
+
+
+    if ((custClassicfication) && _.isEmpty(cusCd) && (custNm) && (status)) {
+        let SQL_WHERE_SEARCH = 'WHERE CUST_NM_ENG LIKE :custNm AND STATUS LIKE :status  AND CUST_GB LIKE :custClassicfication ';
+        sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            custNm,
+            custClassicfication,
+            status,
+            currentLocation,
+            limitRow
+        };
+        let sqlSearch = SQL_SELECT_COUNT + SQL_FROM + SQL_WHERE_SEARCH;
+        let paramSearch = {
+            custNm,
+            custClassicfication,
+            status,
+        };
+        let totalRow;
+        let rowRs;
+        totalRow = await oracelService.queryGetTotalRow(res, sqlSearch, paramSearch, optionFormatObj);
+        rowRs = await oracelService.queryGetTotalRow(res, sql, param, optionFormatObj);
+        return res.status(200).send({count: totalRow, rowRs: rowRs});
+    }
+
+
+    if ((custClassicfication) && (cusCd) && _.isEmpty(custNm) && (status)) {
+        let SQL_WHERE_SEARCH = 'WHERE CUST_CD LIKE :cusCd AND STATUS LIKE :status  AND CUST_GB LIKE :custClassicfication ';
+        sql = SQL_SELECT + SQL_FROM + SQL_WHERE_SEARCH + SQL_ORDER_BY + SQL_LIMIT;
+        let param = {
+            cusCd,
+            custClassicfication,
+            status,
+            currentLocation,
+            limitRow
+        };
+        let sqlSearch = SQL_SELECT_COUNT + SQL_FROM + SQL_WHERE_SEARCH;
+        let paramSearch = {
+            cusCd,
+            custClassicfication,
+            status,
         };
         let totalRow;
         let rowRs;
@@ -205,6 +389,7 @@ exports.addCust = async function (req, res) {
     var validEndDT = (_.isEmpty(req.body.validEndDT)) ? null: req.body.validEndDT.replace(/[^0-9 ]/g, "");
     var operationDate = req.body.operationDate.replace(/[^0-9 ]/g, "");;
     var userID = req.body.userID;
+    var status = 1;
     let param = {
         classFication: { val: classFication },
         custCD: { val: custCD },
@@ -220,10 +405,11 @@ exports.addCust = async function (req, res) {
         validStartDT: { val: validStartDT },
         validEndDT: { val: validEndDT },
         operationDate: { val: operationDate },
-        userID: { val: userID }
+        userID: { val: userID },
+        status: {val: status}
     };
-    var SQL = `INSERT INTO TB_ITCUST(CUST_GB, CUST_CD ,CUST_NM, CUST_NM_ENG, BRANCH_NM, BRANCH_NM_ENG, CO_RGST_NO, BIZ_CG_CD, PRT_CUST_GB, PRT_CUST_CD, ADDR, VALID_START_DT, VALID_END_DT, SYS_DTIM, WORK_ID) VALUES (:classFication, :custCD, :custNM, :custNMENG, :custBranchNM, :custBranchNM_EN, :coRgstNo, :industryCD, :prtOrganizationClass, :prtOrganizationCD, :addr, :validStartDT, :validEndDT, :operationDate, :userID)`;
-    oracelService.queryOracel(res, SQL, param, optionAutoCommit);
+    var SQL = `INSERT INTO TB_ITCUST(CUST_GB, CUST_CD ,CUST_NM, CUST_NM_ENG, BRANCH_NM, BRANCH_NM_ENG, CO_RGST_NO, BIZ_CG_CD, PRT_CUST_GB, PRT_CUST_CD, ADDR, VALID_START_DT, VALID_END_DT, SYS_DTIM, WORK_ID, STATUS) VALUES (:classFication, :custCD, :custNM, :custNMENG, :custBranchNM, :custBranchNM_EN, :coRgstNo, :industryCD, :prtOrganizationClass, :prtOrganizationCD, :addr, :validStartDT, :validEndDT, :operationDate, :userID, :status)`;
+    await oracelService.queryOracel(res, SQL, param, optionAutoCommit);
 };
 
 exports.editCust = async function (req, res) {
@@ -239,7 +425,8 @@ exports.editCust = async function (req, res) {
     var prtOrganizationCD = req.body.prtOrganizationCD;
     var addr = req.body.addr;
     var validEndDT = (_.isEmpty(req.body.validEndDT)) ? null: req.body.validEndDT.replace(/[^0-9 ]/g, "");
-    
+    var status = req.body.status;
+
     var param = {
         classFication: { val: classFication },
         custCD: { val: custCD },
@@ -253,9 +440,10 @@ exports.editCust = async function (req, res) {
         prtOrganizationCD: { val: prtOrganizationCD },
         addr: { val: addr },
         validEndDT: { val: validEndDT },
+        status: {val: status}
     };
-    var SQL = `UPDATE TB_ITCUST SET CUST_NM = :custNM , CUST_NM_ENG = :custNMENG, BRANCH_NM = :custBranchNM, BRANCH_NM_ENG = :custBranchNM_EN, CO_RGST_NO = :coRgstNo, BIZ_CG_CD = :industryCD , PRT_CUST_GB = :prtOrganizationClass, PRT_CUST_CD = :prtOrganizationCD, ADDR = :addr, VALID_END_DT = :validEndDT WHERE CUST_GB = :classFication AND CUST_CD = :custCD `;
-    oracelService.queryOracel(res, SQL, param, optionAutoCommit);
+    var SQL = `UPDATE TB_ITCUST SET CUST_NM = :custNM , CUST_NM_ENG = :custNMENG, BRANCH_NM = :custBranchNM, BRANCH_NM_ENG = :custBranchNM_EN, CO_RGST_NO = :coRgstNo, BIZ_CG_CD = :industryCD , PRT_CUST_GB = :prtOrganizationClass, PRT_CUST_CD = :prtOrganizationCD, ADDR = :addr, VALID_END_DT = :validEndDT , STATUS = :status WHERE CUST_GB = :classFication AND CUST_CD = :custCD `;
+    await oracelService.queryOracel(res, SQL, param, optionAutoCommit);
 };
 
 
--- API/BackOfficeAPI/routes/auth.route.js
+++ API/BackOfficeAPI/routes/auth.route.js
@@ -6,8 +6,8 @@ var router = express.Router();
 var auth_controller = require('../controllers/auth.controller');
 
 router.post('/login',  auth_controller.login);
-router.post('/sendEmail',  auth_controller.sendEmail);
-router.post('/getCodeEmail',  auth_controller.getCodeEmail);
+// router.post('/sendEmail',  auth_controller.sendEmail);
+// router.post('/getCodeEmail',  auth_controller.getCodeEmail);
 router.post('/register',  auth_controller.register);
 module.exports = router;
 
