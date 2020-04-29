7c876c8179959d6e805b76a1b802b93beeff0474
dthoanginfoplus <hoangdt@infoplus.com>
2020-04-29 10:35:51 +0700

    Set role access manage customer for account

--- API/BackOfficeAPI/routes/customer.route.js
+++ API/BackOfficeAPI/routes/customer.route.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var verifyToken = require('../util/verifyToken');
+var verifyAdmin = require('../util/verifyAdmin');
 
 
 var customer_router = require('../controllers/customer.controller');
 
 router.get('/getCustInfo',  verifyToken , customer_router.getCustInfo);
-router.post('/addCust',  verifyToken ,customer_router.addCust);
-router.put('/editCust', verifyToken , customer_router.editCust);
+router.post('/addCust',  verifyAdmin ,customer_router.addCust);
+router.put('/editCust', verifyAdmin, customer_router.editCust);
 module.exports = router;
 
