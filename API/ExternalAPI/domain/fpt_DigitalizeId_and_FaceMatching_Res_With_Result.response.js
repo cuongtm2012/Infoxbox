c7039a1fffa8464b6798e503af4f07182f058034
HoangInfoplus <dthoang@infoplusvn.com>
2020-12-17 11:38:19 +0700

    Fix response FPT_ID_&_FACE

--- API/ExternalAPI/domain/fpt_DigitalizeId_and_FaceMatching_Res_With_Result.response.js
+++ API/ExternalAPI/domain/fpt_DigitalizeId_and_FaceMatching_Res_With_Result.response.js
@@ -1,4 +1,5 @@
 const matchingNumber = 0.95;
+const date = require('date-and-time');
 module.exports = function FptDigitalizeIdAndFaceMatchingResponseWithResult(Request, preResponse, frontImage, rearImage, resultFaceMatching) {
     const {
         responseMessage,
@@ -25,14 +26,58 @@ module.exports = function FptDigitalizeIdAndFaceMatchingResponseWithResult(Reque
     this.idType = idType ? idType : "";
     this.infoProvConcent = infoProvConcent ? infoProvConcent : "";
     this.niceSessionKey = niceSessionKey ? niceSessionKey : "";
-    this.responseTime  = responseTime ? responseTime : "";
+    this.responseTime = responseTime ? responseTime : "";
     this.responseCode = responseCode ? responseCode : "";
     this.responseMessage = responseMessage ? responseMessage : "";
-    this.frontImage = frontImage ? frontImage : "";
-    this.rearImage = rearImage ? rearImage : "";
+    this.frontImage = {
+        idNumber: frontImage.id,
+        name: frontImage.name,
+        birthday: date.transform(frontImage.dob, 'DD-MM-YYYY', 'YYYYMMDD'),
+        sex: convertSexString(frontImage.sex),
+        nationality: frontImage.nationality,
+        home: frontImage.home,
+        address: frontImage.address,
+        type_new: frontImage.type_new,
+        address_entities: {
+            province: frontImage.address_entities.province,
+            district: frontImage.address_entities.district,
+            ward: frontImage.address_entities.ward,
+            street: frontImage.address_entities.street
+        },
+        doe: date.transform(frontImage.doe, 'DD-MM-YYYY', 'YYYYMMDD'),
+        type: frontImage.type
+    };
+    this.rearImage = {
+        ethnicity: rearImage.ethnicity,
+        religion: rearImage.religion,
+        type_new: rearImage.type_new,
+        features: rearImage.features,
+        issue_date: rearImage.issue_date,
+        issue_loc: rearImage.issue_loc,
+        type: rearImage.type
+    };
     this.faceMatchingResult = {
-        similarity: resultFaceMatching.similarity,
+        similarity: truncateFloat(resultFaceMatching.similarity),
         sourceResult: resultFaceMatching.result,
         finalResult: parseFloat(resultFaceMatching.similarity) >= matchingNumber ? 'SAME' : 'DIFFERENT'
     };
+}
+
+function truncateFloat(number) {
+    let value = Number(number);
+    if (value === 0 || value === 1) {
+        return value;
+    } else {
+        return value.toPrecision(5);
+    }
+}
+
+function convertSexString(sex) {
+    if (sex.toLowerCase() === 'nam') {
+        return 'M';
+    } else if (sex.toLowerCase() === 'nu') {
+        return 'F'
+    } else {
+        return '';
+    }
 }
