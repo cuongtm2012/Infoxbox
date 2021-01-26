b2a5d64febe377a9b3c54fca579e3f8cc3f38e7d
HoangInfoplus <dthoang@infoplusvn.com>
2021-01-26 09:50:56 +0700

    Fix SMS content

--- API/SMS_Service_Inbox/domain/OTP.class.js
+++ API/SMS_Service_Inbox/domain/OTP.class.js
@@ -5,13 +5,7 @@ class OTP {
         this.session_id = session_id;
         this.BrandName = BrandName;
         this.Phone = Phone;
-        this.Message = Buffer.from(config.FinalSMS_1 +
-                                    config.TemSMS.APPLE_LINK +
-                                    config.FinalSMS_2 +
-                                    config.TemSMS.ANDROID_LINK +
-                                    config.FinalSMS_3 +
-                                    config.TemSMS.MOBILE_LINK + Phone +
-                                    config.FinalSMS_4).toString('base64');
+        this.Message = Buffer.from(config.FinalSMS).toString('base64');
     }
 }
 
