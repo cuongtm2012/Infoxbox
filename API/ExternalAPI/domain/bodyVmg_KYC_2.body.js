72eca140af9cfebd0b3f77133c5e862f07452617
HoangInfoplus <dthoang@infoplusvn.com>
2021-01-28 16:05:03 +0700

    add call VMG KYC 2 and changee input Rclips main score

---
+++ API/ExternalAPI/domain/bodyVmg_KYC_2.body.js
@@ -0,0 +1,10 @@
+const config = require('../config/config');
+module.exports = function bodyPostVmgKYC2(natId) {
+    this.cmd = 'kyc2_nice';
+    this.idCard = natId;
+    this.idCard1 = "";
+    this.tinCode = "";
+    this.name = "";
+    this.token = config.VmgToken.Dev_Token;
+    this.serviceCode = 'kyc2_nice'
+}
