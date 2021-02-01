c599f199b854273e0197044acded71e8edd65dc0
hi <hoang>
2021-02-01 16:07:02 +0700

    fix body sending contract

--- API/ExternalAPI/domain/bodySubmitInformationEContract.body.js
+++ API/ExternalAPI/domain/bodySubmitInformationEContract.body.js
@@ -2,5 +2,5 @@ module.exports = function bodySubmitInformationEContract(request) {
     this.templateId = request.templateId;
     this.alias = request.alias;
     this.syncType = request.syncType;
-    this.datas = request.datas;
+    this.datas = [request.datas];
 }
