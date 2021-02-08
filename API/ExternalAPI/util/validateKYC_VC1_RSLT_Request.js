974f75c4b26561e98128d5357eae87e899671865
luannt2410 <luannt@infoplusvn.com>
2021-02-08 17:12:25 +0700

    finish KYC_VC1_RSLT

---
+++ API/ExternalAPI/util/validateKYC_VC1_RSLT_Request.js
@@ -0,0 +1,96 @@
+const responcodeEXT = require('../../shared/constant/responseCodeExternal');
+const _ = require('lodash');
+const checkContains = require('../../shared/util/checkcontains');
+module.exports = {
+    checkParamRequest: function (getDataReq) {
+        var response;
+        //fiSessionKey,appNumber
+        if (!_.isEmpty(getDataReq.appNumber) && 20 < getDataReq.appNumber.length) {
+            response = {
+                responseMessage: responcodeEXT.RESCODEEXT.FISessionKeyOverLength.name,
+                responseCode: responcodeEXT.RESCODEEXT.FISessionKeyOverLength.code
+            };
+            return response;
+        }
+        // fiCode
+        if (_.isEmpty(getDataReq.fiCode)) {
+            response = {
+                responseMessage: responcodeEXT.RESCODEEXT.NIFICODE.name,
+                responseCode: responcodeEXT.RESCODEEXT.NIFICODE.code
+            };
+            return response;
+        }
+        if (10 < getDataReq.fiCode.length) {
+            response = {
+                responseMessage: responcodeEXT.RESCODEEXT.FiCodeOverLength.name,
+                responseCode: responcodeEXT.RESCODEEXT.FiCodeOverLength.code
+            };
+            return response;
+        }
+        // Task code
+        if (_.isEmpty(getDataReq.taskCode)) {
+            response = {
+                responseMessage: responcodeEXT.RESCODEEXT.NITASKCODE.name,
+                responseCode: responcodeEXT.RESCODEEXT.NITASKCODE.code
+            };
+            return response;
+        }
+        if (!_.isEqual(responcodeEXT.TaskCode.KYC_VC1_RSLT.code, getDataReq.taskCode)) {
+            response = {
+                responseMessage: responcodeEXT.RESCODEEXT.InvalidTaskCode.name,
+                responseCode: responcodeEXT.RESCODEEXT.InvalidTaskCode.code
+            };
+            return response;
+        }
+        if (30 < getDataReq.taskCode.length) {
+            response = {
+                responseMessage: responcodeEXT.RESCODEEXT.TaskCodeOverLength.name,
+                responseCode: responcodeEXT.RESCODEEXT.TaskCodeOverLength.code
+            };
+            return response;
+        }
+        //    phone number
+        if (_.isEmpty(getDataReq.mobilePhoneNumber)) {
+            response = {
+                responseMessage: responcodeEXT.RESCODEEXT.NIMOBILEPHONENUMBER.name,
+                responseCode: responcodeEXT.RESCODEEXT.NIMOBILEPHONENUMBER.code
+            };
+            return response;
+        }
+        if (200 < getDataReq.mobilePhoneNumber.length) {
+            response = {
+                responseMessage: responcodeEXT.RESCODEEXT.NIMOBILEPHONENUMBER.name,
+                responseCode: responcodeEXT.RESCODEEXT.NIMOBILEPHONENUMBER.code
+            };
+            return response;
+        }
+        //nicesessionkey
+        if (_.isEmpty(getDataReq.niceSessionKey)) {
+            response = {
+                responseMessage: responcodeEXT.RESCODEEXT.NINICESESSIONKEY.name,
+                responseCode: responcodeEXT.RESCODEEXT.NINICESESSIONKEY.code
+            }
+            return response;
+        }
+        // infoProvConcent
+        if (_.isEmpty(getDataReq.infoProvConcent)) {
+            response = {
+                responseMessage: responcodeEXT.RESCODEEXT.ConsentProvisionIsNotValid.name,
+                responseCode: responcodeEXT.RESCODEEXT.ConsentProvisionIsNotValid.code
+            };
+            return response;
+        }
+        if (!checkContains.contains.call(responcodeEXT.InfoProvConcent, getDataReq.infoProvConcent.toUpperCase())) {
+            response = {
+                responseMessage: responcodeEXT.RESCODEEXT.ConsentProvisionIsNotValid.name,
+                responseCode: responcodeEXT.RESCODEEXT.ConsentProvisionIsNotValid.code
+            };
+            return response;
+        }
+
+        else
+            response = {};
+
+        return response;
+    }
+};
