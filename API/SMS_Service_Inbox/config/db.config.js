2a95d8c0f98559a64e7b1689f3bc594e8014bbb9
hi <hoang>
2021-03-20 13:43:47 +0700

    change pool connection SMS service

---
+++ API/SMS_Service_Inbox/config/db.config.js
@@ -0,0 +1,28 @@
+const oracledb = require('oracledb');
+const dbconfig = require('../../shared/config/dbconfig');
+const config = require('./config')
+
+const dbOption = {
+	user: dbconfig.user,
+	password: dbconfig.password,
+	connectString: dbconfig.connectString,
+	poolAlias: config.poolAlias,
+	poolMax: 2000, // maximum size of the pool
+	poolMin: 0, // let the pool shrink completely
+	poolIncrement: 1, // only grow the pool by one connection at a time
+	poolTimeout: 0  // never terminate idle connections
+}
+
+async function initialize() {
+	//create oracle connection pool
+	await oracledb.createPool(
+			dbOption,
+			function (err, pool) {
+				if (err) {
+					console.error("createPool() error: " + err.message);
+					throw err;
+				}
+			}
+		);//end oracledb.createpool
+}
+module.exports.initialize = initialize;
