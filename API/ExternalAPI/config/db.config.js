5e3920afbaf90ce144675504c6f9a09cf8f74d25
hi <hoang>
2021-03-24 15:42:58 +0700

    eror message

--- API/ExternalAPI/config/db.config.js
+++ API/ExternalAPI/config/db.config.js
@@ -7,23 +7,22 @@ const dbOption = {
 	password: dbconfig.password,
 	connectString: dbconfig.connectString,
 	poolAlias: config.poolAlias,
-	poolMax: 10, // maximum size of the pool
-	poolMin: 10, // let the pool shrink completely
-	poolIncrement: 0, // only grow the pool by one connection at a time
-	poolTimeout: 60  // never terminate idle connections
+	poolMax: 4, // maximum size of the pool
+	poolMin: 0, // let the pool shrink completely
+	poolIncrement: 1, // only grow the pool by one connection at a time
+	poolTimeout: 0  // never terminate idle connections
 }
 
 async function initialize() {
 	//create oracle connection pool
 	await oracledb.createPool(
-			dbOption,
-			function (err, pool) {
-				if (err) {
-					console.error("createPool() error: " + err.message);
-					throw err;
-				}
+		dbOption,
+		function (err, pool) {
+			if (err) {
+				console.error("createPool() error: " + err.message);
+				throw err;
 			}
-		);//end oracledb.createpool
+		}
+	);//end oracledb.createpool
 }
 module.exports.initialize = initialize;
-module.exports.dbOption = dbOption;
