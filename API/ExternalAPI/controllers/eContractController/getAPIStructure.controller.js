8e74f1ecc6d3d3bb6528f28d206d0473e12fc3b6
tuanma-nice <tuanma@nicegroup.com.vn>
2021-03-29 12:52:38 +0700

    sss

--- API/ExternalAPI/controllers/eContractController/getAPIStructure.controller.js
+++ API/ExternalAPI/controllers/eContractController/getAPIStructure.controller.js
@@ -54,25 +54,26 @@ exports.getStructureAPI = function(req, res) {
 					logger.info(responseData);
 					return res.status(500).json(responseData);
 				} else {
-					console.log("dns.lookup checking..." + URI.URL_E_CONTRACT_GET_TOKEN_ACCESS_DEV);
-					let statusErrorDNS = dnsLookup('demo.econtract.fpt.com.vn')
-					if(statusErrorDNS){
-						console.log("dns errorrrrrrrrrrrrrrr 1111111111 sleep 2s");
-						await sleep(2000);
-						console.log("dns check again");
-						statusErrorDNS = dnsLookup('demo.econtract.fpt.com.vn')
+					(async() => {
+						console.log("dns.lookup checking..." + URI.URL_E_CONTRACT_GET_TOKEN_ACCESS_DEV);
+						let statusErrorDNS = dnsLookup('demo.econtract.fpt.com.vn')
 						if(statusErrorDNS){
-							console.log("dns errorrrrrrrrrrrrrrr 22222222222 stop");
-							return res.status(500).json({error: statusErrorDNS});
+							console.log("dns errorrrrrrrrrrrrrrr 1111111111 sleep 2s");
+							await sleep(2000);
+							console.log("dns check again");
+							statusErrorDNS = dnsLookup('demo.econtract.fpt.com.vn')
+							if(statusErrorDNS){
+								console.log("dns errorrrrrrrrrrrrrrr 22222222222 stop");
+								return res.status(500).json({error: statusErrorDNS});
+							}else{
+								console.log("getAuthAccess lan 2");
+								return getAuthAccess(config,req, res)
+							}
 						}else{
-							console.log("eContractProcess lan 2");
-							return eContractProcess(config,req, res)
+							console.log("getAuthAccess lan 1");
+							return getAuthAccess(config,req, res)
 						}
-					}else{
-						console.log("eContractProcess lan 1");
-						return eContractProcess(config,req, res)
-					}
-					
+					})();
 					//Tuanma move to function
 
 				}
@@ -88,75 +89,35 @@ exports.getStructureAPI = function(req, res) {
 	}
 }
 
-function eContractProcess(config, req, res) {
+function getAuthAccess(config, req, res) {
 	//    getAuthAccess
 	let bodyGetAuth = new bodyGetAuthEContract();
 
 	axios.post(URI.URL_E_CONTRACT_GET_TOKEN_ACCESS_DEV, bodyGetAuth, config).then(
 		resultGetAuthAccess => {
 			if (!_.isEmpty(resultGetAuthAccess.data.access_token)) {
-				let URlGetStructureContract = URI.URL_E_CONTRACT_GET_STRUCTURE_API_DEV + req.query.alias;
-				let configGetStructure = {
-					headers: {
-						'Authorization': `Bearer ${resultGetAuthAccess.data.access_token}`,
-						'Accept-Encoding': 'gzip, deflate, br'
-					},
-					timeout: 60 * 1000
-				}
-				axios.get(URlGetStructureContract, configGetStructure).then(
-					resultGetStructure => {
-						if (resultGetStructure.status === 200 && !_.isEmpty(resultGetStructure.data)) {
-							preResponse = new PreResponse(responCode.RESCODEEXT.NORMAL.name, '', dateutil.timeStamp(), responCode.RESCODEEXT.NORMAL.code);
-							responseData = new responseGetApiStructureResponseWithResult(req.query, preResponse, resultGetStructure.data);
-							dataInqLogSave = new DataSaveToInqLog(req.query, preResponse);
-							cicExternalService.insertDataToINQLOG(dataInqLogSave).then();
-							cicExternalService.updateRspCdScrapLogAfterGetResult('', responCode.RESCODEEXT.NORMAL.code).then();
-							logger.info(responseData);
-							return res.status(200).json(responseData);
-						} else {
-							//    update scraplog & response F048
-							console.log('errGetStructure: ', resultGetStructure);
-							preResponse = new PreResponse(responCode.RESCODEEXT.EXTITFERR.name, '', dateutil.timeStamp(), responCode.RESCODEEXT.EXTITFERR.code);
-							responseData = new getApiStructureResponseWithoutResult(req.query, preResponse);
-							dataInqLogSave = new DataSaveToInqLog(req.query, preResponse);
-							cicExternalService.insertDataToINQLOG(dataInqLogSave).then();
-							cicExternalService.updateRspCdScrapLogAfterGetResult('', responCode.RESCODEEXT.EXTITFERR.code).then();
-							logger.info(responseData);
-							logger.info(resultGetStructure.data);
-							return res.status(200).json(responseData);
+				(async() => {
+					console.log("222 dns.lookup checking..." + URI.URL_E_CONTRACT_GET_STRUCTURE_API_DEV);
+					let statusErrorDNS = dnsLookup('demo.econtract.fpt.com.vn')
+					if(statusErrorDNS){
+						console.log("222 dns errorrrrrrrrrrrrrrr 1111111111 sleep 2s");
+						await sleep(2000);
+						console.log("222 dns check again");
+						statusErrorDNS = dnsLookup('demo.econtract.fpt.com.vn')
+						if(statusErrorDNS){
+							console.log("222 dns errorrrrrrrrrrrrrrr 22222222222 stop");
+							return res.status(500).json({error: statusErrorDNS});
+						}else{
+							console.log("222 getStructureContract lan 2");
+							return getStructureContract(resultGetAuthAccess,req, res)
 						}
-					}).catch(reason => {
-						console.log('errGetStructureContract: ', reason.toString());
-						if (reason.response && reason.response.data.message === 'Internal Server Error: template  is not exists') {
-							console.log('errGetStructureContract: ', reason.response.data.message);
-							preResponse = new PreResponse(responCode.RESCODEEXT.NoContractTemplateForInputAlias.name, '', dateutil.timeStamp(), responCode.RESCODEEXT.NoContractTemplateForInputAlias.code);
-							responseData = new getApiStructureResponseWithoutResult(req.query, preResponse);
-							dataInqLogSave = new DataSaveToInqLog(req.query, preResponse);
-							cicExternalService.insertDataToINQLOG(dataInqLogSave).then();
-							cicExternalService.updateRspCdScrapLogAfterGetResult('', responCode.RESCODEEXT.NoContractTemplateForInputAlias.code).then();
-							logger.info(responseData);
-							logger.info(reason.toString());
-							return res.status(200).json(responseData);
-						} else if (reason.message === 'timeout of 60000ms exceeded') {
-							preResponse = new PreResponse(responCode.RESCODEEXT.EXTITFTIMEOUTERR.name, '', dateutil.timeStamp(), responCode.RESCODEEXT.EXTITFTIMEOUTERR.code);
-							responseData = new getApiStructureResponseWithoutResult(req.query, preResponse);
-							dataInqLogSave = new DataSaveToInqLog(req.query, preResponse);
-							cicExternalService.insertDataToINQLOG(dataInqLogSave).then();
-							cicExternalService.updateRspCdScrapLogAfterGetResult('', responCode.RESCODEEXT.EXTITFTIMEOUTERR.code).then();
-							logger.info(responseData);
-							logger.info(reason.toString());
-							return res.status(200).json(responseData);
-						} else {
-							preResponse = new PreResponse(responCode.RESCODEEXT.EXTITFERR.name, '', dateutil.timeStamp(), responCode.RESCODEEXT.EXTITFERR.code);
-							responseData = new getApiStructureResponseWithoutResult(req.query, preResponse);
-							dataInqLogSave = new DataSaveToInqLog(req.query, preResponse);
-							cicExternalService.insertDataToINQLOG(dataInqLogSave).then();
-							cicExternalService.updateRspCdScrapLogAfterGetResult('', responCode.RESCODEEXT.EXTITFERR.code).then();
-							logger.info(responseData);
-							logger.info(reason.toString());
-							return res.status(200).json(responseData);
-						}
-					})
+					}else{
+						console.log("222 getStructureContract lan 1");
+						return getStructureContract(resultGetAuthAccess,req, res)
+					}
+				})();
+					
+				
 			}
 		}
 	).catch(reason => {
@@ -181,6 +142,71 @@ function eContractProcess(config, req, res) {
 			return res.status(200).json(responseData);
 		}
 	})
+}
+
+function getStructureContract(resultGetAuthAccess, req, res){
+	let URlGetStructureContract = URI.URL_E_CONTRACT_GET_STRUCTURE_API_DEV + req.query.alias;
+	let configGetStructure = {
+		headers: {
+			'Authorization': `Bearer ${resultGetAuthAccess.data.access_token}`,
+			'Accept-Encoding': 'gzip, deflate, br'
+		},
+		timeout: 60 * 1000
+	}
+	axios.get(URlGetStructureContract, configGetStructure).then(
+		resultGetStructure => {
+			if (resultGetStructure.status === 200 && !_.isEmpty(resultGetStructure.data)) {
+				preResponse = new PreResponse(responCode.RESCODEEXT.NORMAL.name, '', dateutil.timeStamp(), responCode.RESCODEEXT.NORMAL.code);
+				responseData = new responseGetApiStructureResponseWithResult(req.query, preResponse, resultGetStructure.data);
+				dataInqLogSave = new DataSaveToInqLog(req.query, preResponse);
+				cicExternalService.insertDataToINQLOG(dataInqLogSave).then();
+				cicExternalService.updateRspCdScrapLogAfterGetResult('', responCode.RESCODEEXT.NORMAL.code).then();
+				logger.info(responseData);
+				return res.status(200).json(responseData);
+			} else {
+				//    update scraplog & response F048
+				console.log('errGetStructure: ', resultGetStructure);
+				preResponse = new PreResponse(responCode.RESCODEEXT.EXTITFERR.name, '', dateutil.timeStamp(), responCode.RESCODEEXT.EXTITFERR.code);
+				responseData = new getApiStructureResponseWithoutResult(req.query, preResponse);
+				dataInqLogSave = new DataSaveToInqLog(req.query, preResponse);
+				cicExternalService.insertDataToINQLOG(dataInqLogSave).then();
+				cicExternalService.updateRspCdScrapLogAfterGetResult('', responCode.RESCODEEXT.EXTITFERR.code).then();
+				logger.info(responseData);
+				logger.info(resultGetStructure.data);
+				return res.status(200).json(responseData);
+			}
+		}).catch(reason => {
+			console.log('errGetStructureContract: ', reason.toString());
+			if (reason.response && reason.response.data.message === 'Internal Server Error: template  is not exists') {
+				console.log('errGetStructureContract: ', reason.response.data.message);
+				preResponse = new PreResponse(responCode.RESCODEEXT.NoContractTemplateForInputAlias.name, '', dateutil.timeStamp(), responCode.RESCODEEXT.NoContractTemplateForInputAlias.code);
+				responseData = new getApiStructureResponseWithoutResult(req.query, preResponse);
+				dataInqLogSave = new DataSaveToInqLog(req.query, preResponse);
+				cicExternalService.insertDataToINQLOG(dataInqLogSave).then();
+				cicExternalService.updateRspCdScrapLogAfterGetResult('', responCode.RESCODEEXT.NoContractTemplateForInputAlias.code).then();
+				logger.info(responseData);
+				logger.info(reason.toString());
+				return res.status(200).json(responseData);
+			} else if (reason.message === 'timeout of 60000ms exceeded') {
+				preResponse = new PreResponse(responCode.RESCODEEXT.EXTITFTIMEOUTERR.name, '', dateutil.timeStamp(), responCode.RESCODEEXT.EXTITFTIMEOUTERR.code);
+				responseData = new getApiStructureResponseWithoutResult(req.query, preResponse);
+				dataInqLogSave = new DataSaveToInqLog(req.query, preResponse);
+				cicExternalService.insertDataToINQLOG(dataInqLogSave).then();
+				cicExternalService.updateRspCdScrapLogAfterGetResult('', responCode.RESCODEEXT.EXTITFTIMEOUTERR.code).then();
+				logger.info(responseData);
+				logger.info(reason.toString());
+				return res.status(200).json(responseData);
+			} else {
+				preResponse = new PreResponse(responCode.RESCODEEXT.EXTITFERR.name, '', dateutil.timeStamp(), responCode.RESCODEEXT.EXTITFERR.code);
+				responseData = new getApiStructureResponseWithoutResult(req.query, preResponse);
+				dataInqLogSave = new DataSaveToInqLog(req.query, preResponse);
+				cicExternalService.insertDataToINQLOG(dataInqLogSave).then();
+				cicExternalService.updateRspCdScrapLogAfterGetResult('', responCode.RESCODEEXT.EXTITFERR.code).then();
+				logger.info(responseData);
+				logger.info(reason.toString());
+				return res.status(200).json(responseData);
+			}
+		})
 }
 
 function sleep(ms) {
