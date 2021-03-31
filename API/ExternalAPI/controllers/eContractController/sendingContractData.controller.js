ee94395cc4599d7d0cc901f30a39857bea8af5f6
hi <hoang>
2021-03-31 13:55:17 +0700

    dns cache

--- API/ExternalAPI/controllers/eContractController/sendingContractData.controller.js
+++ API/ExternalAPI/controllers/eContractController/sendingContractData.controller.js
@@ -19,6 +19,10 @@ const bodySendInformationEContract = require('../../domain/bodySubmitInformation
 const BodyPostRiskScore = require('../../domain/body_Post_RiskScore.body');
 const bodyVmg_KYC_2 = require('../../domain/bodyVmg_KYC_2.body');
 const httpClient = require('../../services/httpClient.service');
+import { registerInterceptor } from 'axios-cached-dns-resolve';
+const axiosClient = axios.create(config);
+registerInterceptor(axiosClient);
+
 exports.sendingContractData = async function (req, res)  {
     try {
         const config = {
@@ -34,11 +38,11 @@ exports.sendingContractData = async function (req, res)  {
 
         let resK2 = {};
         let bodyRiskScore = new BodyPostRiskScore(test);
-        const resRiskScore = await axios.post(URI.URL_VMG_DEV, bodyRiskScore , config).then(
+        const resRiskScore = await axiosClient.post(URI.URL_VMG_DEV, bodyRiskScore , config).then(
             async result => {
                 console.log(result.data);
                 let bodyK2 = new bodyVmg_KYC_2(test.natId);
-                resK2 = await axios.post(URI.URL_VMG_DEV, bodyK2, config).then(
+                resK2 = await axiosClient.post(URI.URL_VMG_DEV, bodyK2, config).then(
                     value => {
                         return value.data;
                         // console.log(value.data);
