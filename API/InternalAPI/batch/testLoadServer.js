2c097771b57e8ab0e5bab56fccbbff8bec1520a4
tuanma-nice <tuanma@nicegroup.com.vn>
2021-03-31 10:49:10 +0700

    sss

--- API/InternalAPI/batch/testLoadServer.js
+++ API/InternalAPI/batch/testLoadServer.js
@@ -29,14 +29,14 @@ let IP_TEST = '103.112.124.153'
 let times = 0
 const statusContractUrl = 'https://'+IP_TEST+':3100/contract/FTN_GAS_RQST?fiCode=B100000011&taskCode=FTN_GAS_RQST&alias=hop_dong_nice_test_0112';
 
-axios.get(statusContractUrl, config).then(
+/*axios.get(statusContractUrl, config).then(
 	     result => {
 	         countNextTimeCron('statusContractUrl');
 	     }
 	     ).catch((error) => {
              countNextTimeCron('statusContractUrl');
             return console.log(error.toString());
-        });
+        });*/
 
 /*
 
@@ -109,6 +109,233 @@ axios.post(nfScore_URL, NfscorePro, config).then(
         });*/
 
 
+const urlCreateContract = 'https://222.252.25.116:3100/contract/FTN_SCD_RQST';
+const bodyCreateContract = {
+    "fiSessionKey": "",
+    "fiCode": "B100000011",
+    "taskCode": "FTN_SCD_RQST",
+    "templateId": "0000124ef8M8UdJR8z58wsRl",
+    "alias": "hop_dong_nice_test_0112",
+    "syncType": "sync",
+    "datas": [
+        {
+            "id": "envName",
+            "name": "Há»£p Ä‘á»“ng NiceTest",
+            "type": "header_fields",
+            "value": "Há»£p Ä‘á»“ng NiceTest",
+            "owner": "envelope_hdr",
+            "dataType": "String",
+            "required": true
+        },
+        {
+            "id": "envNo",
+            "name": "0000001",
+            "type": "header_fields",
+            "value": "",
+            "owner": "envelope_hdr",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "envDate",
+            "name": "NgÃ y kÃ½",
+            "type": "header_fields",
+            "value": "2021-01-12T05:00:00.000Z",
+            "owner": "envelope_hdr",
+            "dataType": "Date",
+            "required": false
+        },
+        {
+            "id": "envSubmittedFrom",
+            "name": " Ä�Æ°á»£c gá»­i tá»« :  ",
+            "type": "header_fields",
+            "value": "/496/517",
+            "owner": "envelope_hdr",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "p_001",
+            "name": "name_party",
+            "type": "p",
+            "value": "NICE Viet Nam",
+            "owner": "party",
+            "dataType": "String",
+            "required": true
+        },
+        {
+            "id": "p_001_r_001",
+            "name": "name_recipient",
+            "type": "r",
+            "value": "Mai Anh Tuáº¥n",
+            "owner": "recipient",
+            "dataType": "String",
+            "required": true
+        },
+        {
+            "id": "p_001_r_001",
+            "name": "mail_recipient",
+            "type": "m",
+            "value": "tuanma@nicegroup.com.vn",
+            "owner": "recipient",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "p_001_r_001",
+            "name": "phone_recipient",
+            "type": "phone",
+            "value": null,
+            "owner": "recipient",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "p_001_r_001",
+            "name": "contact_recipient",
+            "type": "contactId",
+            "value": null,
+            "owner": "recipient",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "p_002",
+            "name": "name_party",
+            "type": "p",
+            "value": "BÃŠN B: BÃŠN VAY",
+            "owner": "party",
+            "dataType": "String",
+            "required": true
+        },
+        {
+            "id": "p_002_r_002",
+            "name": "name_recipient",
+            "type": "r",
+            "value": "Mr.Han",
+            "owner": "recipient",
+            "dataType": "String",
+            "required": true
+        },
+        {
+            "id": "p_002_r_002",
+            "name": "mail_recipient",
+            "type": "m",
+            "value": "jhhan@nicegroup.com.vn",
+            "owner": "recipient",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "p_002_r_002",
+            "name": "phone_recipient",
+            "type": "phone",
+            "value": null,
+            "owner": "recipient",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "p_002_r_002",
+            "name": "contact_recipient",
+            "type": "contactId",
+            "value": null,
+            "owner": "recipient",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "p_003",
+            "name": "name_party",
+            "type": "p",
+            "value": "BÃŠN C: OK VAY",
+            "owner": "party",
+            "dataType": "String",
+            "required": true
+        },
+        {
+            "id": "p_003_r_003",
+            "name": "name_recipient",
+            "type": "r",
+            "value": "OK Vay Tuan ky",
+            "owner": "recipient",
+            "dataType": "String",
+            "required": true
+        },
+        {
+            "id": "p_003_r_003",
+            "name": "mail_recipient",
+            "type": "m",
+            "value": "tuanmait@gmail.com",
+            "owner": "recipient",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "p_003_r_003",
+            "name": "phone_recipient",
+            "type": "phone",
+            "value": null,
+            "owner": "recipient",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "p_003_r_003",
+            "name": "contact_recipient",
+            "type": "contactId",
+            "value": null,
+            "owner": "recipient",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "0dd8f8413034a26bb50",
+            "name": "fullnameA",
+            "type": "ff",
+            "value": "Mai Anh Tuáº¥n",
+            "owner": "requester",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "d6d53b64ed768c59eca",
+            "name": "fullnameB",
+            "type": "ff",
+            "value": "Ä�á»— VÄƒn Han",
+            "owner": "requester",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "edc5c854ad5e38c7f94",
+            "name": "addressC",
+            "type": "ff",
+            "value": "54 Liá»…u Giai, Ba Ä�Ã¬nh, HÃ  Ná»™i",
+            "owner": "requester",
+            "dataType": "String",
+            "required": false
+        },
+        {
+            "id": "dueDays",
+            "name": "due_in_days",
+            "type": "due_days",
+            "value": "30",
+            "owner": "envelope",
+            "dataType": "Number",
+            "required": false
+        }
+    ]
+}
+
+axios.post(urlCreateContract, bodyCreateContract, config).then(
+        result => {
+            countNextTimeCron("urlCreateContract");
+        }
+    ).catch((error) => {
+        countNextTimeCron(1);
+        return console.log(error.toString());
+    });
 
         function countNextTimeCron(any) {
             times++;
