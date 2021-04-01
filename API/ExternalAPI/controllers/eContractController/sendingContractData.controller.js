a7cce6f80eb64f24d127612555d715f8bd7961b4
hi <hoang>
2021-04-01 17:14:47 +0700

    cấds

--- API/ExternalAPI/controllers/eContractController/sendingContractData.controller.js
+++ API/ExternalAPI/controllers/eContractController/sendingContractData.controller.js
@@ -51,37 +51,24 @@ export function sendingContractData(req, res) {
             mobilePhoneNumber: '0964785596',
             natId: '001096002249'
         }
-
-        let resK2 = {};
         let bodyRiskScore = new BodyPostRiskScore(test);
-        const resRiskScore = axiosClient.post(URI.URL_VMG_DEV, bodyRiskScore , config).then(
-            async result => {
-                console.log(result.data);
+        axiosClient.post(URI.URL_VMG_DEV, bodyRiskScore , config).then(
+            result => {
                 let bodyK2 = new bodyVmg_KYC_2(test.natId);
-                resK2 = await axiosClient.post(URI.URL_VMG_DEV, bodyK2, config).then(
+                axiosClient.post(URI.URL_VMG_DEV, bodyK2, config).then(
                     value => {
-                        return value.data;
-                        // console.log(value.data);
-                        // return res.status(200).send(value.data);
+                        console.log('ok')
+                        return res.status(200).send('ok');
                     }
                 ).catch(reason => {
                     console.log(reason);
                     return res.status(500).send(reason.toString())
                 });
-                return result.data;
             }
         ).catch(reason => {
             console.log(reason);
             return res.status(500).send(reason.toString());
         });
-
-
-        const resRisk = {
-            resK2,
-            resRiskScore
-        };
-        console.log(resRisk);
-        return res.end();
     } catch (err) {
         console.log(err);
         return res.status(500).json({error: err.toString()});
