54f23d8115fad19721459f8570df69d495e6f207
hi <hoang>
2021-04-02 17:49:59 +0700

    Add axios service

--- API/ExternalAPI/controllers/eContractController/sendingContractData.controller.js
+++ API/ExternalAPI/controllers/eContractController/sendingContractData.controller.js
@@ -15,9 +15,10 @@
 import {axiosPost} from '../../services/httpClient.service.js';
 // const bodyGetAuthEContract = require('../../domain/bodyGetAuthEContract.body')
 // const bodySendInformationEContract = require('../../domain/bodySubmitInformationEContract.body')
-import BodyPostRiskScore from '../../domain/body_Post_RiskScore.body.js';
 import bodyVmg_KYC_2 from '../../domain/bodyVmg_KYC_2.body.js';
 import URI from '../../../shared/URI.js';
+const urlGetAuth = 'account/unauth/v1/login'
+import configExternal from '../../config/config.js';
 export function sendingContractData(req, res) {
     try {
         const config = {
@@ -26,15 +27,17 @@ export function sendingContractData(req, res) {
             },
             timeout: 60 * 10000
         }
+        let body = {
+            username: configExternal.AccountFptDev.username,
+            password: configExternal.AccountFptDev.password
+        }
         const test = {
             mobilePhoneNumber: '0964785596',
             natId: '001096002249'
         }
-        let bodyRiskScore = new BodyPostRiskScore(test);
-        axiosPost(URI.URL_VMG_DEV, bodyRiskScore , config).then(
+        axiosPost(URI.URL_FPT_DEV + urlGetAuth, body, config).then(
             result => {
-                let bodyK2 = new bodyVmg_KYC_2(test.natId);
-                axiosPost(URI.URL_VMG_DEV, bodyK2, config).then(
+                axiosPost(URI.URL_FPT_DEV + urlGetAuth, body, config).then(
                     value => {
                         console.log('ok')
                         return res.status(200).send('ok');
