0454789c1b0bb7641299dabacccb355ae800eefb
hi <hoang>
2021-04-03 11:08:40 +0700

    change API getStructureContract to ES6

--- API/ExternalAPI/routes/eContract.route.js
+++ API/ExternalAPI/routes/eContract.route.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 import { sendingContractData } from '../controllers/eContractController/sendingContractData.controller.js';
 import { statusOfContract } from '../controllers/eContractController/statusOfCotract.controller.js';
-// import getStructureAPIController from'../controllers/eContractController/getAPIStructure.controller';
+import { getStructureAPI } from'../controllers/eContractController/getAPIStructure.controller.js';
 // import contractDownloadApiController from'../controllers/eContractController/contractDownloadApi.controller';
 
 
 router.post('/FTN_SCD_RQST', sendingContractData);
 
 router.get('/FTN_CCS_RQST', statusOfContract);
-//
-// router.get('/FTN_GAS_RQST', getStructureAPIController.getStructureAPI);
+
+router.get('/FTN_GAS_RQST', getStructureAPI);
 //
 // router.get('/FTN_GCT_RQST', contractDownloadApiController.contractDownloadApi);
 export default router;
