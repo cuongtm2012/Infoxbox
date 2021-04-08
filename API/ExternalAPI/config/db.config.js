d71730c0986ee8a8cf37adcea62a651291abe92b
hi <hoang>
2021-04-08 09:02:16 +0700

    fix pool db

--- API/ExternalAPI/config/db.config.js
+++ API/ExternalAPI/config/db.config.js
@@ -1,40 +1,59 @@
 const oracledb = require('oracledb');
 const dbconfig = require('../../shared/config/dbconfig');
 const config = require('./config')
-
+const SQL = `SELECT COUNT (*) AS COUNT FROM tb_inqlog WHERE sys_dtim BETWEEN ('20210405000000') AND ('20210407000000')`;
 const dbOption = {
-	_enableStats  : true,
-	user: dbconfig.user,
-	password: dbconfig.password,
-	connectString: dbconfig.connectString,
-	poolAlias: config.poolAlias,
-	poolMax: 10,
-	poolMin: 10,
-	poolIncrement: 0,
-	poolTimeout: 0
+    _enableStats: true,
+    user: dbconfig.user,
+    password: dbconfig.password,
+    connectString: dbconfig.connectString,
+    poolAlias: config.poolAlias,
+    poolMax: 10,
+    poolMin: 10,
+    poolIncrement: 0,
+    poolTimeout: 0
 }
 let poolInfo;
+
 async function initialize() {
-	//create oracle connection pool
-	await oracledb.createPool(
-		dbOption,
-		function (err, pool) {
-			if (err) {
-				console.error("createPool() error: " + err.message);
-				throw err;
-			}
-			setInterval(function(){
-				pool.getConnection(function(e,c){
-					poolInfoFnc();
-					c.release(); });
-			}, 60000);
-			poolInfo = pool;
-		}
-	);//end oracledb.createpool
+    //create oracle connection pool
+    try {
+        await oracledb.createPool(
+            dbOption,
+            function (err, pool) {
+                if (err) {
+                    console.error("createPool() error: " + err.message);
+                    throw err;
+                }
+
+                setInterval(async function () {
+                    pool.getConnection(async function (e, c) {
+                        let result = await c.execute(
+                            // The statement to execute
+                            SQL,
+                            {},
+                            {
+                                outFormat: oracledb.OUT_FORMAT_OBJECT
+                            });
+                        console.log(result.rows[0].COUNT);
+                        c.release();
+                    });
+                }, 30000);
+                poolInfo = pool;
+            }
+        );//end oracledb.createpool
+    } catch (e) {
+        if (poolInfo) {
+            poolInfo.close();
+        }
+        await initialize();
+        console.log(e.toString());
+    }
 }
 
 function poolInfoFnc() {
-	console.log(poolInfo._logStats());
+    console.log(poolInfo._logStats());
 }
+
 module.exports.initialize = initialize;
 module.exports.poolInfoFnc = poolInfoFnc;
