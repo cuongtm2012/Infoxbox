c19e0fcb54107504171bc64497debfec4c09b30a
hi <hoang>
2021-04-14 13:24:41 +0700

    fix input Rclips

--- API/ExternalAPI/domain/nfScorePost.Body.js
+++ API/ExternalAPI/domain/nfScorePost.Body.js
@@ -1,8 +1,8 @@
 const dateutil = require('../util/dateutil');
-module.exports = function NfScorePostBody(phoneNumber, natID, VMG_Score, VMG_Grade, Zalo_Score) {
+module.exports = function NfScorePostBody(fiCode, niceSsKey,phoneNumber, natID, VMG_Score, VMG_Grade, Zalo_Score) {
     this.serviceName = 'OKF_CSS_ASS';
-    this.custNo = dateutil.getCurrentInquiryDate();
-    this.recvNo = dateutil.timeStamp();
+    this.custNo = fiCode;
+    this.recvNo = niceSsKey;
     this.listInput = {
         A0001: phoneNumber,
         A0002: natID,
