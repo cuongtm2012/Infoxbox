dc64f0f7f21da5577d3da09d71dcc05f871722cb
hi <hoang>
2021-05-10 15:41:10 +0700

    add check duplicate request NF score

--- API/ExternalAPI/services/cicExternal.service.js
+++ API/ExternalAPI/services/cicExternal.service.js
@@ -6,6 +6,7 @@ const niceGoodCode = require('../../shared/util/niceGoodCode');
 const ipGateWay = require('../../shared/util/getIPGateWay');
 const _ = require('lodash');
 const responCode = require('../../shared/constant/responseCodeExternal');
+const moment = require('moment');
 async function insertSCRPLOG(req) {
     let connection;
 
@@ -1952,6 +1953,67 @@ async function updateScrpStatCdErrorResponseCodeScraping(niceSessionKey, code, n
     }
 }
 
+async function selectRecordDuplicateIn24h(phoneNumber,nationalId) {
+    let connection;
+
+    try {
+        let timeGetRequest = moment().format('YYYYMMDDHHmmss')
+        let yesterday = moment().subtract(1, 'days').format('YYYYMMDDHHmmss');
+        let objResult = {};
+        let sql, result;
+
+        connection = await oracledb.getConnection(config.poolAlias);
+
+
+        sql = `SELECT a.*
+               FROM tb_ext_score a
+                        INNER JOIN tb_inqlog b on a.tel_no_mobile = b.tel_no_mobile
+               Where a.tel_no_mobile = :phoneNumber
+                 and b.natl_id = :nationalId
+                 and a.score_cd = 'NOK100_001'
+                 and b.rsp_cd = 'P000'
+                 and a.system_dtim BETWEEN (:yesterday) and (:timeGetRequest)
+               order by a.system_dtim DESC
+                   FETCH NEXT 1 ROWS ONLY`;
+
+        result = await connection.execute(
+            // The statement to execute
+            sql,
+            {
+                phoneNumber: { val: phoneNumber },
+                nationalId: { val: nationalId },
+                yesterday: { val: yesterday },
+                timeGetRequest: { val: timeGetRequest }
+            },
+            {outFormat: oracledb.OUT_FORMAT_OBJECT},
+        );
+        console.log(result.rows);
+        if (result.rows[0]) {
+            result.rows.forEach(
+                element => {
+                    objResult.niceSessionKey = element.NICE_SSIN_ID;
+                    objResult.nfGrade = element.GRADE;
+                    objResult.cutoffResult = element.FINAL_APPROVAL;
+                });
+            return objResult;
+        } else {
+            return {}
+        }
+    } catch (err) {
+        console.log(err);
+        throw err;
+        // return res.status(400);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+}
+
 module.exports.insertSCRPLOG = insertSCRPLOG;
 module.exports.insertINQLOG = insertINQLOG;
 module.exports.selectCICS11aRSLT = selectCICS11aRSLT;
@@ -1984,3 +2046,4 @@ module.exports.insertDataToExtScore = insertDataToExtScore;
 module.exports.updateScrapingTranslog = updateScrapingTranslog;
 module.exports.updateCICReportInquiryCompleted = updateCICReportInquiryCompleted;
 module.exports.updateScrpStatCdErrorResponseCodeScraping = updateScrpStatCdErrorResponseCodeScraping;
+module.exports.selectRecordDuplicateIn24h = selectRecordDuplicateIn24h;
